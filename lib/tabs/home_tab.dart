470687f02ae324daa1f3f2931eb3fda9830dc327
Shrey Shourya <shouryashrey@gmail.com>
2021-07-19 02:58:14 +0530

    Product Images are now loading from the databse

--- lib/tabs/home_tab.dart
+++ lib/tabs/home_tab.dart
@@ -1,7 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:pages/constants.dart';
 import 'package:pages/widgets/custom_action_bar.dart';
-import '../constants.dart';
 
 class HomeTab extends StatelessWidget {
   final CollectionReference _productsRef =
@@ -23,11 +23,42 @@ class HomeTab extends StatelessWidget {
                 );
               }
 
-              if (snapshot.connectionState == ConnectionState.active) {
-                return Scaffold(
-                  body: Center(
-                    child: Text("Ban gya connection"),
-                  ),
+              // Data is ready to display on the Scaffold
+              if (snapshot.connectionState == ConnectionState.done) {
+                return ListView(
+                  padding: EdgeInsets.only(top: 75.0, bottom: 12.0),
+                  children: snapshot.data.docs.map((document) {
+                    return Container(
+                      margin: EdgeInsets.symmetric(
+                          vertical: 12.0, horizontal: 40.0),
+                      child: Column(
+                        children: [
+                          ClipRRect(
+                            borderRadius: BorderRadius.circular(20.0),
+                            child: Image.network(
+                              "${document.data()["images"][0]}",
+                            ),
+                          ),
+                          SizedBox(
+                            height: 10.0,
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Text(
+                                "${document.data()["name"]}",
+                                style: Constants.boldHeading,
+                              ),
+                              Text(
+                                "Rs. ${document.data()["price"]}",
+                                style: Constants.regularDarkText,
+                              )
+                            ],
+                          )
+                        ],
+                      ),
+                    );
+                  }).toList(),
                 );
               }
 
